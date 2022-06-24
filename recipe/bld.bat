07d0a74a2bc571fbae7416f13c649a3de2d56f2f
Kai Tietz <ktietz@anaconda.com>
2022-06-24 13:47:35 +0200

    adjust Windows build, which failed to set proper MAJ_MIN_VER

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,14 +2,11 @@ cd w%ARCH%
 copy config_VC config.h
 
 :: Get the major minor version info (e.g. `4_60`)
-python -c "import os; print('_'.join(os.environ['PKG_VERSION'].split('.')[:2]))" > temp.txt
-set /p MAJ_MIN_VER=<temp.txt
-
+set MAJ_MIN_VER="4_65"
 nmake /f Makefile_VC
 nmake /f Makefile_VC_DLL
 
-dir *.exe *.dll
-echo "version-maj-min: %MAJ_MIN_VER%"
+set MAJ_MIN_VER="4_65"
 copy glpsol.exe %LIBRARY_BIN%
 copy ..\src\glpk.h %LIBRARY_INC%
 copy glpk.lib %LIBRARY_LIB%
